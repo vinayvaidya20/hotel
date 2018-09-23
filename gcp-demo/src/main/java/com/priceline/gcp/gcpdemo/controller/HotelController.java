cac19f9e5803b76bae096901fde685bf858d658a
vinayvaidya20 <vinay.vaidya20@gmail.com>
2018-09-23 15:15:11 -0400

    Update HotelController.java

--- gcp-demo/src/main/java/com/priceline/gcp/gcpdemo/controller/HotelController.java
+++ gcp-demo/src/main/java/com/priceline/gcp/gcpdemo/controller/HotelController.java
@@ -73,7 +73,7 @@ public class HotelController {
 	@RequestMapping(method = RequestMethod.GET, value = "/")
 	public String getResult() throws SQLException {
 	
-		return "SUCCESS-Rolling Update Done";
+		return "SUCCESS";
 	}
 	
 	@RequestMapping(method = RequestMethod.GET, value = "/con")
