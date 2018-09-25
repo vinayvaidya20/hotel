361edaf7814541986197f084f70a83a014e630e9
vinayvaidya20 <vinay.vaidya20@gmail.com>
2018-09-25 10:28:37 -0400

    Update HotelController.java

--- gcp-demo/src/main/java/com/priceline/gcp/gcpdemo/controller/HotelController.java
+++ gcp-demo/src/main/java/com/priceline/gcp/gcpdemo/controller/HotelController.java
@@ -73,7 +73,7 @@ public class HotelController {
 	@RequestMapping(method = RequestMethod.GET, value = "/")
 	public String getResult() throws SQLException {
 	
-		return "TestSUCCESS";
+		return "SUCCESS";
 	}
 	
 	@RequestMapping(method = RequestMethod.GET, value = "/con")
